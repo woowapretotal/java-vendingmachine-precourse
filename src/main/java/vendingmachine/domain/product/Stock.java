f1791a3271858a75a55f8d6e9e399e75da038eee
Junseo Bae <ferrater1013@gmail.com>
2026-01-07 15:34:43 +0900

    feat: 재고 차감 및 검증 기능 구현

---
+++ src/main/java/vendingmachine/domain/product/Stock.java
@@ -0,0 +1,43 @@
+package vendingmachine.domain.product;
+
+import vendingmachine.common.error.DomainException;
+import vendingmachine.common.error.ErrorMessage;
+
+import java.util.Objects;
+
+public class Stock {
+    private static final int MIN_VALUE = 0;
+
+    private final int stockQuantity;
+
+    public Stock(final int stockQuantity) {
+        validateMinRange(stockQuantity);
+        this.stockQuantity = stockQuantity;
+    }
+
+    private void validateMinRange(final int value) {
+        if (value < MIN_VALUE) {
+            throw new DomainException(ErrorMessage.INSUFFICIENT_STOCK, MIN_VALUE);
+        }
+    }
+
+    public Stock withDecreaseQuantity(int decreaseQuantity) {
+        return new Stock(this.stockQuantity - decreaseQuantity);
+    }
+
+    public int value() {
+        return stockQuantity;
+    }
+
+    @Override
+    public boolean equals(final Object object) {
+        if (object == null || getClass() != object.getClass()) return false;
+        Stock integerMaxVO = (Stock) object;
+        return stockQuantity == integerMaxVO.stockQuantity;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(stockQuantity);
+    }
+}
